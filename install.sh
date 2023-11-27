b8c9e9b0df7f71d76d51e58d3eea5686b2b4827f
Bertan Karacora <bertan.karacora@gmail.com>
2023-11-27 23:25:51 +0100

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # For local development:
-# Fork repository
+# Fork repository.
 # Install git, configure git and ssh keys / access token / ...
-# Clone repo
+# Clone repo.
 
+sudo add-apt-repository ppa:deadsnakes/
+sudo apt update
 cat requirements_apt.txt | xargs sudo apt install -y
-python3 -m venv .venv
+
+python3.8 -m venv .venv
 source .venv/bin/activate
 
 pip install --upgrade pip
@@ -17,9 +20,18 @@ pip install --no-cache-dir -e .
 git clone https://github.com/MRtrix3/mrtrix3.git
 cd mrtrix3
 ./configure
+# You might need:
+# NUMBER_OF_PROCESSORS=1
 ./build
 ./set_path
+# Check via:
+# dwi2response -help
 
 # You probably also need to install FSL (https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FSL).
-# For this, register yourself at their website.
+# It is free, you need to register at their website.
+# Run:
 # python fslinstaller.py
+# You might need:
+# export FSLDIR=~/TractSeg/fsl
+# Check via:
+# bet -help
