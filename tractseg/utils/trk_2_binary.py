1027de99e89b1186e09deb7ff3521b2c93681d15
Bertan Karacora <bertan.karacora@gmail.com>
2023-12-12 06:34:34 +0100

    Add options to binarization script

--- tractseg/utils/trk_2_binary.py
+++ tractseg/utils/trk_2_binary.py
@@ -1,13 +1,8 @@
 import argparse
 import dipy.tracking.utils as tracking_utils
-import logging
 import nibabel as nib
 import numpy as np
 import scipy as sp
-
-# Set formatting of output
-logging.basicConfig(format="%(levelname)s: %(message)s")
-logging.getLogger().setLevel(logging.INFO)
 
 
 def parse_args():
@@ -17,20 +12,17 @@ def parse_args():
             Example usage: python trk_2_binary.py -i CST_right.trk -o CST_right.nii.gz -ref nodif_brain_mask.nii.gz
         """
     )
+    parser.add_argument("-i", dest="path_input", required=True)
+    parser.add_argument("-o", dest="path_output", required=True)
+    parser.add_argument("--ref", dest="path_reference", required=True)
+    # Not recommended (might remove valid parts).
+    parser.add_argument("--remove_blobs", action="store_true", default=False)
+    # Not recommended (not ideal because it tends to remove valid holes, e.g., in MCP).
+    parser.add_argument("--close_holes", action="store_true", default=False)
+    parser.add_argument("--subsegment_factor", type=float, default=0.25)
 
-    parser.add_argument("-i", metavar="filepath", dest="input", required=True)
-    parser.add_argument("-o", metavar="filepath", dest="output", required=True)
-    parser.add_argument("--ref", metavar="filepath", dest="reference", required=True)
-    parser.add_argument("--legacy_format", action="store_true", help="Use for zenodo dataset v1.1.0 and below", default=False)
     args = parser.parse_args()
     return args
-
-
-def get_number_of_points(streamlines):
-    count = 0
-    for sl in streamlines:
-        count += len(sl)
-    return count
 
 
 def remove_small_blobs(img, threshold=1):
@@ -38,59 +30,45 @@ def remove_small_blobs(img, threshold=1):
     Find blobs/clusters of same label. Only keep blobs with more than threshold elements.
     This can be used for postprocessing.
     """
-    # If using structure=np.ones((3, 3, 3): Also considers diagonal elements for determining if a element belongs to a blob
-    # -> not good, because leaves hardly any small blobs we can remove.
-    mask, number_of_blobs = sp.ndimage.label(img)
-    counts = np.bincount(mask.flatten())
+    labels, num_labels = sp.ndimage.label(img)
+    counts_labels = np.bincount(labels.flatten())
 
-    logging.debug(f"Number of blobs before filtering: {number_of_blobs}")
-    logging.debug(f"Pixel counts: {counts}")
+    labels_to_remove = np.nonzero(counts_labels <= threshold)[0]
+    for label_to_remove in labels_to_remove:
+        img[labels == label_to_remove] = 0
 
-    remove = np.nonzero(counts <= threshold)[0]
-    for idx in remove:
-        mask[mask == idx] = 0
-    mask[mask > 0] = 1
-
-    mask_after, number_of_blobs_after = sp.ndimage.label(mask)
-    logging.debug(f"Number of blobs after filtering: {number_of_blobs_after}")
-    return mask
+    return img
 
 
 def main():
     args = parse_args()
 
-    # Read files.
-    if args.legacy_format:
-        streams, hdr = nib.trackvis.read(args.input)
-        streamlines = [s[0] for s in streams]
-    else:
-        file_sl = nib.streamlines.load(args.input)
-        streamlines = file_sl.streamlines
-    img_ref = nib.load(args.reference)
+    file_tracts = nib.streamlines.load(args.path_input)
+    img_ref = nib.load(args.path_reference)
 
-    # Upsample Streamlines (very important, especially when using DensityMap Threshold, without upsampling eroded results).
-    len_max_seq = abs(img_ref.affine[0, 0] / 4)
-    streamlines = list(tracking_utils.subsegment(streamlines, len_max_seq))
+    # Upsample Streamlines
+    # Very important, especially when using DensityMap Threshold.
+    zooms = np.asarray(img_ref.header.get_zooms())
+    len_segment_max = np.min(zooms) * args.subsegment_factor
+    streamlines = list(tracking_utils.subsegment(file_tracts.streamlines, len_segment_max))
 
-    # Remember: Does not count if a fibers has no node inside of a voxel -> upsampling helps, but not perfect.
-    # Counts the number of unique streamlines that pass through each voxel -> oversampling does not distort result.
-    dm = tracking_utils.density_map(streamlines, affine=img_ref.affine, vol_dims=img_ref.get_fdata().shape)
+    # Does not count if a fibers has no node inside of a voxel, upsampling helps, but is not perfect.
+    # Counts the number of unique streamlines that pass through each voxel, so oversampling does not distort the result.
+    dm = tracking_utils.density_map(streamlines, affine=img_ref.affine, vol_dims=img_ref.shape)
 
-    # Create Binary Map
-    # Using higher Threshold problematic, because tends to remove valid parts (sparse fibers).
-    dm_binary = dm > 0
+    # Using higher threshold problematic, because tends to remove valid parts (sparse fibers).
+    threshold = 1
+    dm_bin = dm >= threshold
 
-    # Filter Blobs (might remove valid parts) -> do not use
-    # dm_binary_c = dm_binary
-    # dm_binary_c = remove_small_blobs(dm_binary_c, threshold=10)
+    if args.remove_blobs:
+        dm_bin = remove_small_blobs(dm_bin, threshold=10)
 
-    # Closing of Holes (not ideal because tends to remove valid holes, e.g. in MCP) -> do not use
-    # size = 1
-    # dm_binary_c = ndimage.binary_closing(dm_binary_c, structure=np.ones((size, size, size))).astype(dm_binary.dtype)
+    if args.close_holes:
+        dm_bin = sp.ndimage.binary_closing(dm_bin, structure=np.ones((1, 1, 1)))
 
-    # Save Binary Mask
-    img_dm_binary = nib.Nifti1Image(dm_binary.astype(np.uint8), img_ref.affine)
-    nib.save(img_dm_binary, args.output)
+    # Nibabel does not support bool dtype.
+    img_dm_binary = nib.Nifti1Image(dm_bin.astype(np.uint8), img_ref.affine)
+    nib.save(img_dm_binary, args.path_output)
 
 
 if __name__ == "__main__":
