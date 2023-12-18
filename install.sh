a4d9fe6c7c8a92c5622ec3dccc53f03666f727fb
Bertan Karacora <bertan.karacora@gmail.com>
2023-12-18 06:04:37 +0100

    Fix bonndit installation and change to python3.9

--- install.sh
+++ install.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
 
-# For local development:
-# Fork repository.
-# Install git, configure git and ssh keys / access token / ...
-# Clone repo.
+num_processors_building_mrtrix3=1
+dir_fsl=~/TractSeg/libs/fsl
 
-sudo add-apt-repository ppa:deadsnakes/
-sudo apt update
-cat env/requirements_apt.txt | xargs sudo apt install -y
+install_system_requirements() {
+    sudo add-apt-repository -y ppa:deadsnakes/ppa
+    sudo apt update
+    sudo apt install -y python3.9 python3.9-venv python-is-python3
+    sudo apt install -y $(cat requirements/requirements_apt.txt)
+}
 
-python3.8 -m venv .venv
-source .venv/bin/activate
+activate_python_venv() {
+    python3.9 -m venv .venv
+    source .venv/bin/activate
+}
 
-pip install --upgrade pip
-pip install wheel
-pip install -r env/requirements_pip.txt
-pip install --no-cache-dir -e .
+install_python_requirements() {
+    pip install --upgrade pip
+    pip install wheel
+    pip install -r requirements/requirements_pip.txt
+    pip install -e .
+}
 
-git clone https://github.com/MRtrix3/mrtrix3.git
-cd mrtrix3
-./configure
-# You might need:
-# NUMBER_OF_PROCESSORS=1
-./build
-./set_path
-# Check via:
-# dwi2response -help
+install_mrtrix3() {
+    export NUMBER_OF_PROCESSORS=$num_processors_building_mrtrix3
+    cd libs
+    git clone https://github.com/MRtrix3/mrtrix3.git
+    cd mrtrix3
+    ./configure
+    ./build
+    ./set_path
+    cd ../..
+}
 
-# You probably also need to install FSL (https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FSL).
-# It is free, you need to register at their website.
-# Run:
-# python fslinstaller.py
-# You might need:
-# export FSLDIR=~/TractSeg/fsl
-# Check via:
-# bet -help
+install_fsl() {
+    export FSLDIR=$dir_fsl
+    cd libs
+    wget https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/releases/fslinstaller.py
+    python fslinstaller.py
+    rm fslinstaller.py
+}
+
+install_explicit_dependencies() {
+    mkdir -p libs
+    install_mrtrix3
+    install_fsl
+}
+
+main() {
+    install_system_requirements
+    activate_python_venv
+    install_python_requirements
+    install_explicit_dependencies
+}
+
+main
